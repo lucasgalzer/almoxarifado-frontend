19321762db2c0ba719aa29723ad6569560e226e1
Lucas Galzer <lucas.galzer@gmail.com>
2026-07-01 16:02:58 -0300

    Refatoração: atualização da chamada da API para lidar com a estrutura de dados do produto e aprimoramento dos estilos CSS para maior consistência da interface do usuário.

--- src/components/ModalManutencao.jsx
+++ src/components/ModalManutencao.jsx
@@ -21,11 +21,11 @@ function ModalManutencao({ onFechar, onSalvar }) {
     observacoes: '',
   })
 
-  useEffect(() => {
-    api.get('/produtos', { params: { tipo: 'reutilizavel' } })
-      .then(({ data }) => setProdutos(data))
-      .catch(console.error)
-  }, [])
+ useEffect(() => {
+  api.get('/produtos', { params: { tipo: 'reutilizavel' } })
+    .then(({ data }) => setProdutos(data.dados || []))
+    .catch(console.error)
+}, [])
 
   function handleChange(e) {
     const { name, value } = e.target
--- src/components/ModalSolicitacao.jsx
+++ src/components/ModalSolicitacao.jsx
@@ -14,9 +14,11 @@ function ModalSolicitacao({ onFechar, onSalvar }) {
     observacoes: '',
   })
 
-  useEffect(() => {
-    api.get('/produtos').then(({ data }) => setProdutos(data)).catch(console.error)
-  }, [])
+ useEffect(() => {
+  api.get('/produtos', { params: { tipo: 'reutilizavel' } })
+    .then(({ data }) => setProdutos(data.dados || []))
+    .catch(console.error)
+}, [])
 
   function handleFormChange(e) {
     const { name, value } = e.target
